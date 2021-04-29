fcabf291bd30b895704281dbb6ab800ff35111b1
Muhammad Azmi <muhammadazmi4456@gmail.com>
2021-04-29 13:33:02 +0700

    membuat nya lebih simpel dengan hanya menambahkan class popup-confirm-delete

--- resources/views/layouts/back-layouts.blade.php
+++ resources/views/layouts/back-layouts.blade.php
@@ -379,5 +379,32 @@
     </script>
     @include('sweetalert::alert')
 
-
+    <script>    
+        $('.popup-confirm-delete').click(function(e) {
+            e.preventDefault();
+            // @yield('sweeturl')
+            var deleteurl = $(this).attr('href');
+            
+            Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            }).then((result) => {
+            console.log(result)
+            if (result.value === true) {
+                Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+                )
+                window.location = deleteurl;
+            } 
+          })
+        })
+    </script>
 </html>
