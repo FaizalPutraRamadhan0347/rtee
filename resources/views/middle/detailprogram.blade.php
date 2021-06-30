f6bcbc42e8293953eb0bb320beb6cbadc251740a
fathanm19 <fathan.mubina19@gmail.com>
2021-06-30 12:09:50 +0700

    Zoom

--- resources/views/middle/detailprogram.blade.php
+++ resources/views/middle/detailprogram.blade.php
@@ -213,6 +213,32 @@ ul { list-style-type: none; }
                             <th>Nominal Donasi</th>
                             <th>Bukti Pembayaran</th>
                           </tr>
+                          <link rel="stylesheet" type="text/css" media="screen" href="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.css" />
+                          <style type="text/css">
+                            a.fancybox img {
+                            border: none;
+                            box-shadow: 0 1px 7px rgba(0,0,0,0.6);
+                            -o-transform: scale(1,1); 
+                            -ms-transform: scale(1,1); 
+                            -moz-transform: scale(1,1); 
+                            -webkit-transform: scale(1,1); 
+                            transform: scale(1,1); 
+                            -o-transition: all 0.2s ease-in-out; 
+                            -ms-transition: all 0.2s ease-in-out; 
+                            -moz-transition: all 0.2s ease-in-out; 
+                            -webkit-transition: all 0.2s ease-in-out; 
+                            transition: all 0.2s ease-in-out;
+                            } 
+                            a.fancybox:hover img {
+                            position: relative; 
+                            z-index: 999; 
+                            -o-transform: scale(1.15,1.15); 
+                            -ms-transform: scale(1.15,1.15); 
+                            -moz-transform: scale(1.15,1.15); 
+                            -webkit-transform: scale(1.15,1.15); 
+                            transform: scale(1.15,1.15);
+                            }
+                          </style>
                         </thead>
                         <tbody>
                           @foreach ($program->donatur as $donatur)
@@ -223,11 +249,34 @@ ul { list-style-type: none; }
                             @if ($donatur->bukti_pembayaran == '')
                                 <td><p class="badge badge-green">Belum Konfirmasi</p></td>
                             @else    
-                                <td> <img class="img-bukti" src="{{$donatur->getFoto()}}"> </td>
+                                <td> <img class="fancybox" src="{{$donatur->getFoto()}}"> </td>
                             @endif
                           
                           </tr>
                           @endforeach
+                          <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+                          <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+                          <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.pack.min.js"></script>
+                          <script type="text/javascript">
+                              $(function($){
+                                var addToAll = false;
+                                var gallery = true;
+                                var titlePosition = 'inside';
+                                $(addToAll ? 'img' : 'img.fancybox').each(function(){
+                                var $this = $(this);
+                                var title = $this.attr('title');
+                                var src = $this.attr('data-big') || $this.attr('src');
+                                var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
+                                $this.wrap(a);
+                              });
+                          if (gallery)
+                            $('a.fancybox').attr('rel', 'fancyboxgallery');
+                            $('a.fancybox').fancybox({
+                            titlePosition: titlePosition
+                          });
+                        });
+                        $.noConflict();
+                        </script>
                         </tbody>
                         @endif
 
@@ -240,8 +289,11 @@ ul { list-style-type: none; }
                         <li class="pagination-item"> <a>2</a></li>
                         <li class="pagination-item"> <a>3</a></li>
                         <li class="pagination-item"> <a>Next</a></li>
+                        
+
                       </ul>
                     </div>
+                    
                   </div>
 
             </div>
